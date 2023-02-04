afda02132d58816411b1f3c958fa168a648e60e4
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2023-02-04 13:18:30 +0000

    Add files via upload

---
+++ Assets/Audio/LevelMusicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMusicController : MonoBehaviour
+{    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "MainMenu")
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
